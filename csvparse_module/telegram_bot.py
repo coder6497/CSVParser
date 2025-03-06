9b97ada9584af5e28d14d997a61413fcfb551c14
Семён Бабушкин <rty.sem@yandex.ru>
2025-03-06 18:25:51 +0300

    added button for select table to delete

--- csvparse_module/telegram_bot.py
+++ csvparse_module/telegram_bot.py
@@ -5,51 +5,42 @@ bot = telebot.TeleBot(token_file.token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=2)
-    btn = types.KeyboardButton("Посмотреть файлы")
-    keyboard.add(btn)
+    keyboard = types.ReplyKeyboardMarkup()
+    show_btn = types.KeyboardButton("Посмотреть файлы")
+    delete_btn = types.KeyboardButton("Удалить файлы")
+    keyboard.add(show_btn, delete_btn)
     bot.send_message(message.chat.id, 'Отправьте файл формата CSV', reply_markup=keyboard)
 
 @bot.message_handler(commands=['get_res'])
 def get_res(message):
     args = message.text.split()[1:]
     if len(args) == 3:
-        filename, delimeter, header = args[0], args[1], args[2]
-        data_table = csvparse.get_data_table(os.path.join('files', filename), delimeter)
-        data_map = csvparse.get_dict_from_table(data_table)
-        result = csvparse.get_parse_result(data_map, header)
-        res_to_show = ''
-        table_to_show = ''
-
-        for key, value in result.items():
-            res_to_show += f'{key} : {value}\n'
-
-        for row in data_table:
-            for elem in row:
-                table_to_show += f'{elem} '
-            table_to_show += '\n'
-
-        bot.reply_to(message, table_to_show)
-        bot.send_message(message.chat.id, res_to_show)
+        try:
+            filename, delimeter, header = args[0], args[1], args[2]
+            data_table = csvparse.get_data_table(os.path.join('files', filename), delimeter)
+            data_map = csvparse.get_dict_from_table(data_table)
+            result = csvparse.get_parse_result(data_map, header)
+            res_to_show = ''
+            for key, value in result.items():
+                res_to_show += f'{key} : {value}\n'
+            bot.reply_to(message, res_to_show)
+        except Exception:
+            bot.reply_to(message, "Неправильный формат данных(возможно неправильный заголовок или разделитель)")
     else:
         bot.reply_to(message, 'Неправильный формат команды /get_res файл разделитель заголовок')
 
 
-@bot.message_handler(commands=['delete_table'])
-def delete_table(message):
-    args = message.text.split()[1:]
-    if len(args) == 1:
-        table_name = args[0]
-        os.remove(os.path.join('files', table_name))
-    else:
-        bot.reply_to(message, "Неправильный формат команды /delete_table файл")
-
-
 @bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+def delete_table(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    list(map(lambda btn: keyboard.add(btn), os.listdir("files")))
+    keyboard.add("/start")
+    if message.text == "Удалить файлы":
+        bot.send_message(message.chat.id, "Выберите то что надо удалить\n", reply_markup=keyboard)
+    if message.text in os.listdir("files"):
+        os.remove(os.path.join('files', message.text))
     if message.text == "Посмотреть файлы":
-        bot.send_message(message.chat.id, 'Список файлов:\n\n' + '\n'.join(os.listdir('files')), reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'Список файлов:\n\n' + '\n'.join(os.listdir('files')))
 
 
 @bot.message_handler(content_types=['document'])
