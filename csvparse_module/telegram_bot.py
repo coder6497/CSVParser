e1ece800b6676fa1de809e4f802bffd2cab83516
Семён Бабушкин <rty.sem@yandex.ru>
2025-03-07 23:08:56 +0300

    added access to data by user id folders

--- csvparse_module/telegram_bot.py
+++ csvparse_module/telegram_bot.py
@@ -1,28 +1,8 @@
-import filecmp
-
 import telebot, os, token_file, csvparse
 from telebot import types
-from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine, insert
-from hashlib import md5
 
-metadata = MetaData()
-
-engine = create_engine('sqlite:///data_base.db')
-
-user_data = Table(
-    'user_data', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('telegram_user_id', String),
-    Column('filename', String)
-)
-
-metadata.create_all(engine)
 
 bot = telebot.TeleBot(token_file.token)
-try:
-    os.mkdir('files')
-except FileExistsError:
-    pass
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -35,10 +15,11 @@ def start(message):
 @bot.message_handler(commands=['getres'])
 def get_res(message):
     args = message.text.split()[1:]
+    user_dir = os.path.join('files', str(message.from_user.id))
     if len(args) == 3:
         try:
             filename, delimeter, header = args[0], args[1], args[2]
-            data_table = csvparse.get_data_table(os.path.join('files', filename), delimeter)
+            data_table = csvparse.get_data_table(os.path.join(user_dir, filename), delimeter)
             data_map = csvparse.get_dict_from_table(data_table)
             result = csvparse.get_parse_result(data_map, header)
             res_to_show = ''
@@ -59,33 +40,32 @@ def get_help(message):
 
 @bot.message_handler(func=lambda message: True)
 def delete_table(message):
+    user_dir = os.path.join('files', str(message.from_user.id))
     keyboard = types.ReplyKeyboardMarkup()
-    user_hash_id = md5(str(message.from_user.id).encode()).hexdigest()
-    list(map(lambda btn: keyboard.add(btn[32:]), os.listdir("files")))
+    list(map(lambda btn: keyboard.add(btn), os.listdir(user_dir)))
     keyboard.add("/start")
     if message.text == "Удалить файлы":
         bot.send_message(message.chat.id, "Выберите то что надо удалить\n", reply_markup=keyboard)
-    if user_hash_id + message.text in os.listdir("files"):
-        os.remove(os.path.join('files', user_hash_id + message.text))
+    if message.text in os.listdir(user_dir):
+        os.remove(os.path.join(user_dir, message.text))
     if message.text == "Посмотреть файлы":
-        bot.send_message(message.chat.id, 'Список файлов:\n\n' + '\n'.join(os.listdir('files'))[32:])
+        bot.send_message(message.chat.id, 'Список файлов:\n\n' + '\n'.join(os.listdir(user_dir)))
 
 
 @bot.message_handler(content_types=['document'])
 def handle_document(message):
+    try:
+        os.mkdir(os.path.join('files', str(message.from_user.id)))
+    except FileExistsError:
+        pass
+    user_dir = os.path.join('files', str(message.from_user.id))
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
     filename = message.document.file_name
-    hash_user_id = md5(str(message.from_user.id).encode()).hexdigest()
-    filename_for_current_user = hash_user_id + filename
     if filename.endswith('.csv'):
-        with open(os.path.join("files", filename_for_current_user), 'wb') as new_file:
+        with open(os.path.join(user_dir, filename), 'wb') as new_file:
             new_file.write(downloaded_file)
         new_file.close()
         bot.reply_to(message, f"Файл {filename} отправлен на сервер")
-    insert_query = insert(user_data).values(telegram_user_id=hash_user_id, filename=filename_for_current_user)
-    with engine.connect() as con:
-        con.execute(insert_query)
-        con.commit()
 
 bot.polling(non_stop=True)
